755530e8434d3adc843e6a55af7dfcd9df448274
Dr. Liew vk <liewvk@yahoo.com>
2026-07-21 11:04:49 +0800

    initial commit Chapter2

---
+++ EnvironmentTest.java
@@ -0,0 +1,7 @@
+    public static void main(String[] args) {
+        System.out.println("Java Development Environment Test");
+        System.out.println("---------------------------------");
+        System.out.println("Java is installed successfully.");
+        System.out.println("The compiler is working.");
+        System.out.println("The program is running correctly.");
+    }
---
+++ SetupReport.java
@@ -0,0 +1,11 @@
+public class SetupReport {
+    public static void main(String[] args) {
+        System.out.println("Java Setup Report");
+        System.out.println("-----------------");
+        System.out.println("Name: Dr. Liew");
+        System.out.println("Operating System: Windows");
+        System.out.println("Editor: Visual Studio Code");
+        System.out.println("IDE: IntelliJ IDEA");
+        System.out.println("Status: Ready to learn Java");
+    }
+}
